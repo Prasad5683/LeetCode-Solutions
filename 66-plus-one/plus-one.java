8fd35c93441f1876526230ea8a901a1e14b3cc59
Prasad Dattatray  Pise <piseprasad5683@gmail.com>
2026-08-20 12:38:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (23.99%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,15 +1,17 @@
 class Solution {
-    public int[] plusOne(int[] nums) {
+    public int[] plusOne(int[] nums ) {
+
         int n = nums.length;
-        for ( int i=n-1;i>=0;i--){
+        for (int i=n-1;i>=0;i--){
             if (nums[i]<9){
                 nums[i]++;
                 return nums;
             }
             nums[i]=0;
         }
-        int res[]= new int [n+1];
+        int res [] = new int [n+1];
         res[0]=1;
-        return res;
+        return res ;
+        
     }
 }
