41b9105eeb8cbfcbce92de7fffd69f12ea1a9983
Prasad Dattatray  Pise <piseprasad5683@gmail.com>
2026-09-06 20:04:50 +0530

    Time: 6 ms (99.73%) | Memory: 47.8 MB (73.88%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int l = 1;
+        int r = 0;
+
+        for (int pile : piles) {
+            r = Math.max(r, pile);
+        }
+
+        int ans = r;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+
+            if (check(mid, piles, h)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static boolean check(int mid, int[] piles, int h) {
+
+        long hours = 0;
+
+        for (int pile : piles) {
+            hours += (pile + mid - 1) / mid;
+        }
+
+        return hours <= h;
+    }
+}
